bf1fd83192c8f7e899cfa1c218654b1b5b9e881c
gamidovich <gamidovich@mail.ru>
2018-11-11 13:42:29 +0300

    Finish Task-2

--- src/index.js
+++ src/index.js
@@ -49,6 +49,19 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
+    if ( !Array.isArray(array) || array.length === 0 ) {
+        throw new Error('empty array');
+    }
+
+    if ( typeof(fn) !== 'function') {
+        throw new Error('fn is not a function');
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if ( fn(array[i]) ) return true;
+    }
+
+    return false;
 }
 
 /*
